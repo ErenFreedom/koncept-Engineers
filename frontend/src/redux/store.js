7a08a68e40105ee51e0c04b363157223bbf66b6a
kartik <kartik@kali.kartik>
2025-06-27 16:22:38 +0530

    Working on DataSetup Hierarchy Part

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import thunk from "redux-thunk"; 
+import {thunk} from "redux-thunk"; 
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
